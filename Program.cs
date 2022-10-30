9555a20a122b5605c2b6d8ad776a576db9469cfb
GiulsV <valente.giulia93@gmail.com>
2022-10-30 14:59:20 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Realizzare un programma in grado di CRIPTARE e DECRIPTARE una stringa inserita nell’utente con la strategia di criptazione nota come “IL CIFRARIO DI CESARE”
+//Mi raccomando ci sono diversi modi di integrarlo, scegliete una strategia semplice in base a quello che abbiamo spiegato:
+//      l’utente inserisce una stringa da criptare / decriptare
+//      l’utente inserisce una chiave numerica per effettuare la criptazione / decriptazione della stringa inserita
